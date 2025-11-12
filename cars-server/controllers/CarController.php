92a3116ed4640d7681a4030a19aedb843335791c
Issa Itani <>
2025-11-12 11:38:54 +0200

    ii - imported charbels code (2)

--- cars-server/controllers/CarController.php
+++ cars-server/controllers/CarController.php
@@ -1,25 +1,29 @@
 <?php
-include("../models/Car.php");
-include("../connection/connection.php");
-include("../services/ResponseService.php");
+require_once(__DIR__ . "/../models/Car.php");
+require_once(__DIR__ . "/../connection/connection.php");
+require_once(__DIR__ . "/../services/ResponseService.php");
 
-function getCars(){
-    global $connection;
+class CarController {
 
-    if(isset($_GET["id"])){
-        $id = $_GET["id"];
-        $car = Car::find($connection, $id);
-        echo ResponseService::response(200, $car->toArray());
-    }else{
-        $cars = Car::findAll($connection);
-        $carsArray = [];
-        foreach($cars as $car){
-            $carsArray[] = $car->toArray();
+    function getCarByID(){
+        global $connection;
+
+        if(isset($_GET["id"])){
+            $id = $_GET["id"];
+        }else{
+            echo ResponseService::response(500, "ID is missing");
+            return;
         }
-        echo ResponseService::response(200, $carsArray);
+       
+        //not allowed to write logic in my controller!!!
+        //$car = Car::find($connection, $id);
+        //$car = $car ? $car->toArray() : [];
+        $car = CarService::findCarByID($id);
+        echo ResponseService::response(200, $car);
+        return;
     }
-}
 
-getCars();
+    //try catch 
+}
 
 ?>
--- cars-server/models/Model.php
+++ cars-server/models/Model.php
@@ -2,12 +2,13 @@
 abstract class Model{
 
     protected static string $table;
-    protected static string $primary_key = "id";
+    //protected static string $primary_key = "id";
 
-    public static function find(mysqli $connection, int $id){
+    public static function find(mysqli $connection, string $id, string $primary_key = "id"){
         $sql = sprintf("SELECT * from %s WHERE %s = ?",
                        static::$table,
-                       static::$primary_key);
+                       $primary_key);
+                       //static::$primary_key);
 
         $query = $connection->prepare($sql);
         $query->bind_param("i", $id);
@@ -19,20 +20,7 @@ abstract class Model{
     }
 
     public static function findAll(mysqli $connection){
-        $sql = sprintf("SELECT * from %s",
-                       static::$table);
-
-        $query = $connection->prepare($sql);
-        $query->execute();               
-
-        $result = $query->get_result();
-        $records = [];
-
-        while($data = $result->fetch_assoc()){
-            $records[] = new static($data);
-        }
-
-        return $records;
+        //implement this
     }
 
 }
